58582810ad1dd9280d1e8b1b03c9696a80fbb005
Shadman Ali <shadman.ali@live.co.uk>
2023-12-06 00:19:27 +0530

    add task service to task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private tasksService: TasksService) {}
+
   @Get('/')
   sayHi() {
     return 'Hello World!';
